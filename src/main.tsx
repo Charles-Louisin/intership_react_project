7e18d2b1ee20c8fc22719593a8d7938baf22aa94
charles <clynlouisin@gmail.com>
2025-03-28 11:27:07 +0100

    mauvais

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
 
+// Désactivation temporaire du StrictMode pour le débogage
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 )
+
+// Ajouter un gestionnaire d'erreurs global
+window.onerror = function(msg, url, lineNo, columnNo, error) {
+  console.log('Erreur globale:', {
+    message: msg,
+    url: url,
+    line: lineNo,
+    column: columnNo,
+    error: error
+  });
+  return false;
+};
+
+// Ajouter un gestionnaire pour les erreurs non gérées des Promises
+window.addEventListener('unhandledrejection', function(event) {
+  console.log('Promesse non gérée:', event.reason);
+});
